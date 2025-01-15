3488110d4b609a00943c932888f047da52598c12
Jonas <jonaselemented@gmail.com>
2025-01-14 19:33:41 -0800

    as

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import { Dashboard } from "@/components/Dashboard";
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
 import { Navbar } from "@/components/Navbar";
-import Image from "next/image";
 
 export default function Home() {
   return (
